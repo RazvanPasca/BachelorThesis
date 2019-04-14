0e5b3dbb982525c0208aef5f6bd32fdcd765f732
gabir <rat_gabriel@yahoo.com>
2019-04-14 10:48:31 +0300

    Added the mouse dataset to the SVM

--- svm.py
+++ svm.py
@@ -1,47 +1,83 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
 from datasets.DATASET_PATHS import GABI_CAT_DATASET_PATH as CAT_DATASET_PATH
-
+from datasets.DATASET_PATHS import GABI_MOUSEACH_DATASET_PATH as MOUSEACH_DATASET_PATH
+from datasets.DATASET_PATHS import GABI_MOUSE_DATASET_PATH as MOUSE_DATASET_PATH
 from datasets.LFPDataset import LFPDataset
 
-dataset = LFPDataset(CAT_DATASET_PATH, normalization='Zsc')
 
-cond_trial_channel = []
-for condition_id in range(0, dataset.number_of_conditions):
-    condition = []
-    for i in range(0, dataset.channels.shape[1] // dataset.trial_length):
-        if dataset.stimulus_conditions[i] == condition_id + 1:
-            trial = dataset.channels[:, (i * dataset.trial_length):((i * dataset.trial_length) + dataset.trial_length)]
-            condition.append(trial)
-    cond_trial_channel.append(np.array(condition))
+def load_cat_dataset(path, label='condition'):
+    dataset = LFPDataset(path, normalization='Zsc')
 
-cond_trial_channel = np.array(cond_trial_channel, dtype=np.float32)
+    x = []
+    y = []
+    for condition_id in range(0, dataset.number_of_conditions):
+        for i in range(0, dataset.channels.shape[1] // dataset.trial_length):
+            if dataset.stimulus_conditions[i] == condition_id + 1:
+                trial = dataset.channels[:,
+                        (i * dataset.trial_length):((i * dataset.trial_length) + dataset.trial_length)]
+                if label == 'condition':
+                    x.append(trial.reshape(-1))
+                    y.append(condition_id)
+                elif label == 'channel':
+                    for i, c in enumerate(trial):
+                        x.append(c)
+                        y.append(i)
+                else:
+                    return None
+    return x, y
 
-cond_trial_channel = cond_trial_channel.reshape((3, 20, -1))
-x = []
-y = []
-for i, movie in enumerate(cond_trial_channel):
-    x.append(cond_trial_channel[i])
-    y.append([i for _ in range(20)])
 
-x = np.array(x).reshape((60, -1))
-y = np.array(y).reshape(60)
+def load_mouse_dataset(path, label='orientation'):
+    dataset = LFPDataset(path, normalization='Zsc')
+    x = []
+    y = []
+    for stimulus_condition in dataset.stimulus_conditions:
+        index = int(stimulus_condition['Trial']) - 1
+        events = [{'timestamp': dataset.event_timestamps[4 * index + i],
+                   'code': dataset.event_codes[4 * index + i]} for i in range(4)]
+        trial = dataset.channels[:, events[1]['timestamp']:(events[1]['timestamp'] + 2672)][:-1]
+        # removed heart rate
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20, shuffle=True)
+        if label == 'orientation':
+            x.append(trial.reshape(-1))
+            y.append((int(stimulus_condition['Condition number']) - 1) // 3)
+        elif label == 'contrast':
+            x.append(trial.reshape(-1))
+            y.append((int(stimulus_condition['Condition number']) - 1) % 3)
+        elif label == 'condition':
+            x.append(trial.reshape(-1))
+            y.append((int(stimulus_condition['Condition number']) - 1))
+        elif label == 'channel':
+            for i, c in enumerate(trial):
+                x.append(c)
+                y.append(i)
+        else:
+            return None
+    return x, y
 
-svclassifier = SVC(kernel='sigmoid')
-svclassifier.fit(X_train, y_train)
 
-print("TEST CONF MATRIX:")
-y_pred = svclassifier.predict(X_test)
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+def compute_confusion_matrix(svc, x, y):
+    y_pred = svc.predict(x)
+    print(confusion_matrix(y, y_pred))
+    print(classification_report(y, y_pred))
 
-print("TRAIN CONF MATRIX:")
-y_pred = svclassifier.predict(X_train)
-print(confusion_matrix(y_train, y_pred))
-print(classification_report(y_train, y_pred))
+
+if __name__ == '__main__':
+    # x, y = load_cat_dataset(CAT_DATASET_PATH, label='condition')
+    x, y = load_mouse_dataset(MOUSEACH_DATASET_PATH, label='orientation')
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
+
+    svc = SVC(kernel='linear')
+    svc.fit(X_train, y_train)
+
+    print("TRAIN CONFUSION MATRIX:")
+    compute_confusion_matrix(svc, X_train, y_train)
+
+    if not len(y_test) is 0:
+        print("TEST CONFUSION MATRIX:")
+        compute_confusion_matrix(svc, X_test, y_test)
