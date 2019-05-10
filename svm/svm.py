d5b2ee3f2d2e8bc6f04146f23080a4c2c2d5af09
gabir <rat_gabriel@yahoo.com>
2019-05-10 09:18:35 +0300

    Added code in svm for scene classification

--- svm/svm.py
+++ svm/svm.py
@@ -1,9 +1,13 @@
+import sys
+
+import numpy as np
+import tensorflow as tf
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
 from datasets.LFPDataset import LFPDataset
-from datasets.paths import MOUSE_DATASET_PATH
+from datasets.paths import CAT_TFRECORDS_PATH_TOBEFORMATED
 
 
 def load_cat_dataset(path, label='condition'):
@@ -78,23 +82,90 @@ def compute_confusion_matrix(svc, x, y):
     print(classification_report(y, y_pred))
 
 
+def load_tf_record(path):
+    reconstructed_data = []
+
+    record_iterator = tf.python_io.tf_record_iterator(path=path)
+
+    for string_record in record_iterator:
+
+        example = tf.train.Example()
+        try:
+            example.ParseFromString(string_record)
+
+            scene = example.features.feature['scene'].bytes_list.value[0]
+
+            trial = int(example.features.feature['trial']
+                        .int64_list
+                        .value[0])
+
+            movie = int(example.features.feature['movie']
+                        .int64_list
+                        .value[0])
+
+            channel = int(example.features.feature['channel']
+                          .int64_list
+                          .value[0])
+
+            signal = (example.features.feature['signal'].float_list.value)
+
+            reconstructed_data.append((scene, trial, movie, channel, signal))
+        except:
+            print("An exception occurred in sample {}".format(string_record))
+
+    return reconstructed_data
+
+
+def load_cat_tf_record(path):
+    cat_scenes_dataset = load_tf_record(path)
+    labels = set([sample[0] for sample in cat_scenes_dataset])
+    labels_to_index = {}
+    for index, label in enumerate(labels):
+        labels_to_index[label] = index
+    x = []
+    y = []
+    for sample in cat_scenes_dataset:
+        x.append(sample[4])
+        y.append(labels_to_index[sample[0]])
+    x = np.array(x)
+    x /= x.max()
+    return x, y, labels_to_index
+
+
 if __name__ == '__main__':
-    # x, y, labels_to_index = load_cat_tf_record("/home/gabir/Repos/wavenet-vae/cat_windows{}.tfrecords".format(1000))
-    # print(labels_to_index)
     # x, y = load_cat_dataset(CAT_DATASET_PATH, label='movie_frame')
-    x, y = load_mouse_dataset(MOUSE_DATASET_PATH, label='orientation', ignore_channels=True)
+    # x, y = load_mouse_dataset(MOUSEACH_DATASET_PATH, label='contrast', ignore_channels=True)
 
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
+    file_redirect = "output.txt"
+    max_iterations = -1
 
-    for i in range(0, 1):
-        print("C={}".format(10 ** i))
-        svc = SVC(kernel='linear', C=10 ** i)
+    if not (file_redirect is None):
+        sys.stdout = open(file_redirect, 'w+')
 
-        svc.fit(X_train, y_train)
+    for window_size in [1000, 800, 400, 200]:
+        x, y, labels_to_index = load_cat_tf_record(CAT_TFRECORDS_PATH_TOBEFORMATED.format(window_size))
+        print(labels_to_index)
 
-        print("TRAIN CONFUSION MATRIX:")
-        compute_confusion_matrix(svc, X_train, y_train)
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
 
-        if not len(y_test) is 0:
-            print("TEST CONFUSION MATRIX:")
-            compute_confusion_matrix(svc, X_test, y_test)
+        for kernel in ["linear", "poly", "rbf"]:
+            for i in np.arange(-1, 2, 0.33):
+                print("Window size={}".format(window_size))
+                print("C={}".format(10 ** i))
+                print("Kernel={}".format(kernel))
+                print("Max iterations={}".format(max_iterations))
+                svc = SVC(kernel=kernel,
+                          C=10 ** i,
+                          max_iter=max_iterations,
+                          cache_size=15000,
+                          class_weight='balanced',
+                          verbose=False)
+
+                svc.fit(X_train, y_train)
+
+                print("TRAIN CONFUSION MATRIX:")
+                compute_confusion_matrix(svc, X_train, y_train)
+
+                if not len(y_test) is 0:
+                    print("TEST CONFUSION MATRIX:")
+                    compute_confusion_matrix(svc, X_test, y_test)
