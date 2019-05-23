6020f032bfb6cef198c3818099b53ca681c52557
gabir <rat_gabriel@yahoo.com>
2019-05-23 10:26:21 +0300

    wavenet with dcgan reconstruction

---
+++ datasets/CatLFPStimuli.py
@@ -0,0 +1,95 @@
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+from scipy.misc import imresize
+from datasets.paths import CAT_DATASET_SIGNAL_PATH
+from datasets.paths import CAT_DATASET_STIMULI_PATH
+
+
+class CatLFPStimuli:
+    def __init__(self,
+                 val_perc=0.20,
+                 test_perc=0.0,
+                 random_seed=42):
+
+        np.random.seed(random_seed)
+
+        self._load_data(CAT_DATASET_SIGNAL_PATH, CAT_DATASET_STIMULI_PATH)
+        self._normalize_data()
+        self.nr_conditions = self.signal.shape[0]
+        self.trials_per_condition = self.signal.shape[1]
+
+        self._split_dataset(val_perc, test_perc)
+
+        np.random.seed(datetime.datetime.now().microsecond)
+
+    def _split_dataset(self, val_perc, test_perc):
+        nr_of_trials = self.nr_conditions * self.trials_per_condition
+        nr_test_trials = round(test_perc * nr_of_trials)
+        nr_val_trials = round(val_perc * nr_of_trials)
+        nr_train_trials = nr_of_trials - (nr_test_trials + nr_val_trials)
+
+        trial_indexes_shuffled = np.arange(0, nr_of_trials)
+        np.random.shuffle(trial_indexes_shuffled)
+        self.train_indexes = trial_indexes_shuffled[:nr_train_trials]
+        self.val_indexes = trial_indexes_shuffled[nr_train_trials:nr_train_trials + nr_val_trials]
+        self.test_indexes = trial_indexes_shuffled[-nr_test_trials:]
+
+        temp_data = self.signal.reshape((60, 47, 28000))
+        self.validation = temp_data[self.val_indexes, :]
+        self.test = temp_data[self.test_indexes, :]
+        self.train = temp_data[self.train_indexes, :]
+
+    def frame_generator(self, frame_size, batch_size, data, data_indexes):
+        x = []
+        y = []
+        while 1:
+            random_sequence, trial_index = self.get_random_sequence(data)
+            batch_start = np.random.choice(range(100, random_sequence.shape[-1] - frame_size - 100))
+            frame = random_sequence[:, batch_start:batch_start + frame_size]
+            image_causing_frame = self._get_stimuli_for_sequence(trial_index, data_indexes, batch_start,
+                                                                 batch_start + frame_size)
+            x.append(frame.reshape(frame_size, 47))
+            y.append(image_causing_frame)
+            if len(x) == batch_size:
+                yield np.array(x), np.array(y)
+                x = []
+                y = []
+
+    def train_frame_generator(self, frame_size, batch_size):
+        return self.frame_generator(frame_size, batch_size, self.train, self.train_indexes)
+
+    def validation_frame_generator(self, frame_size, batch_size):
+        return self.frame_generator(frame_size, batch_size, self.validation, self.val_indexes)
+
+    def test_frame_generator(self, frame_size, batch_size):
+        return self.frame_generator(frame_size, batch_size, self.test, self.test_indexes)
+
+    def get_random_sequence(self, data_source):
+        trial_index = np.random.choice(data_source.shape[0])
+        return data_source[trial_index], trial_index
+
+    def _load_data(self, signal_path, stimuli_path):
+        self.signal = np.load(signal_path)
+        self.stimuli = np.load(stimuli_path)
+
+    def _get_stimuli_for_sequence(self, trial_index, data_indexes, seq_start, seq_end):
+        index_in_all_trials = data_indexes[trial_index]
+        condition = index_in_all_trials // self.trials_per_condition
+        image_number = (seq_start - 100) // 40
+        return self.stimuli[condition, image_number, :, :]
+
+    def _normalize_data(self):
+        for channel in range(self.signal.shape[2]):
+            self.signal[:, :, channel, :] /= np.max(self.signal[:, :, channel, :])
+
+
+if __name__ == '__main__':
+    dataset = CatLFPStimuli()
+    for x, y in dataset.train_frame_generator(100, 2):
+        print(x.shape)
+        print(y.shape)
+        plt.imshow(y[0])
+        plt.show()
---
+++ wavenet_dcgan.py
@@ -0,0 +1,95 @@
+from keras import losses, Input, Model, optimizers
+from keras.activations import softmax
+from keras.layers import Conv1D, Multiply, Add, Activation, Flatten, Dense, K, Reshape, Lambda, BatchNormalization, \
+    UpSampling2D, Conv2D, Conv2DTranspose
+from keras.regularizers import l2
+
+
+def wavenet_block(n_filters, filter_size, dilation_rate, regularization_coef, first=False):
+    def f(input_):
+        if first:
+            residual = Lambda(lambda x: x[:, :, 0], output_shape=(1,))(input_)
+            residual = Reshape(target_shape=(-1, 1))(residual)
+        else:
+            residual = input_
+
+        tanh_out = Conv1D(filters=n_filters,
+                          kernel_size=filter_size,
+                          dilation_rate=dilation_rate,
+                          padding='causal',
+                          activation='tanh',
+                          kernel_regularizer=l2(regularization_coef),
+                          name="Tanh_{}".format(dilation_rate))(input_)
+
+        sigmoid_out = Conv1D(filters=n_filters,
+                             kernel_size=filter_size,
+                             dilation_rate=dilation_rate,
+                             padding='causal',
+                             activation='sigmoid',
+                             kernel_regularizer=l2(regularization_coef),
+                             name="Sigmoid_{}".format(dilation_rate))(input_)
+
+        merged = Multiply(name="Gate_{}".format(dilation_rate))([tanh_out, sigmoid_out])
+
+        skip_out = Conv1D(filters=n_filters * 2,
+                          kernel_size=1,
+                          padding='causal',
+                          kernel_regularizer=l2(regularization_coef),
+                          name="Skip_Conv_{}".format(dilation_rate))(merged)
+
+        out = Conv1D(filters=n_filters,
+                     kernel_size=1,
+                     padding='causal',
+                     kernel_regularizer=l2(regularization_coef),
+                     name="Res_Conv_{}".format(dilation_rate))(merged)
+
+        full_out = Add(name="Block_{}".format(dilation_rate))([out, residual])
+        return full_out, skip_out
+
+    return f
+
+
+def get_wavenet_dcgan_model(nr_filters, input_shape, nr_layers, lr, loss, clipvalue, skip_conn_filters,
+                      regularization_coef, nr_output_classes, multiloss_weights=None, img_size = 64, generator_filter_size=64):
+
+    input_ = Input(shape=input_shape)
+    A, B = wavenet_block(nr_filters, 2, 1, regularization_coef=regularization_coef, first=True)(input_)
+    skip_connections = [B]
+
+    for i in range(1, nr_layers):
+        dilation_rate = 2 ** i
+        A, B = wavenet_block(nr_filters, 2, dilation_rate, regularization_coef=regularization_coef)(A)
+        skip_connections.append(B)
+
+    net = Add(name="Skip_Merger")(skip_connections)
+    net = Activation('relu')(net)
+    net = Conv1D(skip_conn_filters, 1, activation='relu', kernel_regularizer=l2(regularization_coef),
+                 name="Skip_FConv_1")(net)
+    net = Conv1D(skip_conn_filters, 1, kernel_regularizer=l2(regularization_coef), name="Skip_FConv_2")(net)
+    net = Flatten()(net)
+
+    def deconv2d(layer_input, filters=256, kernel_size=(5, 5), strides=(2, 2), bn_relu=True):
+        """Layers used during upsampling"""
+        # u = UpSampling2D(size=2)(layer_input)
+        u = Conv2DTranspose(filters, kernel_size=kernel_size, strides=strides, padding='same')(layer_input)
+        if bn_relu:
+            u = BatchNormalization(momentum=0.9)(u)
+            u = Activation('relu')(u)
+        return u
+
+    generator = Dense(16 * generator_filter_size * img_size // 16 * img_size // 16, activation="relu")(net)
+    generator = Reshape((img_size // 16, img_size // 16,  generator_filter_size * 16))(generator)
+    generator = BatchNormalization()(generator)
+    generator = Activation('relu')(generator)
+    generator = deconv2d(generator, filters=generator_filter_size * 8)
+    generator = deconv2d(generator, filters=generator_filter_size * 4)
+    generator = deconv2d(generator, filters=generator_filter_size * 2)
+    generator = deconv2d(generator, filters=generator_filter_size)
+    generator = deconv2d(generator, filters=1, kernel_size=(3, 3), strides=(1, 1), bn_relu=False)
+
+    output = Activation('tanh')(generator)
+
+    model = Model(inputs=input_, outputs=output)
+    optimizer = optimizers.adam(lr=lr, clipvalue=clipvalue)
+    model.compile(loss=losses.MSE, optimizer=optimizer)
+    return model
