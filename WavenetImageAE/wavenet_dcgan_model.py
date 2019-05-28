2d3bfef8997a0d323d4dd4ad5f18fdbf508a8d96
pasca <razvan_baws@yahoo.com>
2019-05-28 11:15:07 +0300

    Implemented distill paper resize-conv tricks. Testing on cluster.

--- WavenetImageAE/wavenet_dcgan_model.py
+++ WavenetImageAE/wavenet_dcgan_model.py
@@ -1,7 +1,24 @@
 from keras import losses, Input, Model, optimizers
+from keras.backend import tf
+from keras.engine import Layer, InputSpec
 from keras.layers import Conv1D, Multiply, Add, Activation, Flatten, Dense, Reshape, Lambda, BatchNormalization, \
-    Conv2DTranspose, LeakyReLU
+    Conv2D, LeakyReLU, UpSampling2D
 from keras.regularizers import l2
+
+
+class ReflectionPadding2D(Layer):
+    def __init__(self, padding=(1, 1), **kwargs):
+        self.padding = tuple(padding)
+        self.input_spec = [InputSpec(ndim=4)]
+        super(ReflectionPadding2D, self).__init__(**kwargs)
+
+    def compute_output_shape(self, s):
+        """ If you are using "channels_last" configuration"""
+        return s[0], s[1] + 2 * self.padding[0], s[2] + 2 * self.padding[1], s[3]
+
+    def call(self, x, mask=None):
+        w_pad, h_pad = self.padding
+        return tf.pad(x, [[0, 0], [h_pad, h_pad], [w_pad, w_pad], [0, 0]], 'REFLECT')
 
 
 def wavenet_block(n_filters, filter_size, dilation_rate, regularization_coef, first=False):
@@ -47,11 +64,13 @@ def wavenet_block(n_filters, filter_size, dilation_rate, regularization_coef, fi
     return f
 
 
-def deconv2d(layer_input, filters=256, kernel_size=(5, 5), strides=(2, 2), regularization_coef=0.0, bn_relu=True):
+def deconv2d(layer_input, filters=256, kernel_size=(5, 5), strides=(1, 1), regularization_coef=0.0, bn_relu=True):
     """Layers used during upsampling"""
-    u = Conv2DTranspose(filters, kernel_size=kernel_size, strides=strides,
-                        kernel_regularizer=l2(regularization_coef),
-                        padding='same')(layer_input)
+    padding = kernel_size[0] // 2
+    u = UpSampling2D((2, 2), interpolation="nearest", data_format="channels_last")(layer_input)
+    u = ReflectionPadding2D((padding, padding))(u)
+    u = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding='valid',
+               kernel_regularizer=l2(regularization_coef))(u)
     if bn_relu:
         u = BatchNormalization(momentum=0.9)(u)
         u = Activation('relu')(u)
@@ -98,7 +117,7 @@ def get_wavenet_dcgan_model(nr_filters, input_shape, nr_layers, lr, loss, clipva
     elif model.upper() == "REGRESSION":
         output = Dense(1, name="Regression")(net)
 
-    optimizer = optimizers.adam(lr=lr, clipvalue=clipvalue)
     model = Model(inputs=input_, outputs=output)
+    optimizer = optimizers.adam(lr=lr, clipvalue=clipvalue)
     model.compile(loss=losses.MSE if loss.lower() == "mse" else losses.MAE, optimizer=optimizer)
     return model
